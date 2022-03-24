ec8d57951b2ca2fdbde14b5753094fec2ad643d0
Luis Felipe <felipe_salinas_15@hotmail.es>
2022-03-23 20:10:50 -0600

    avaces

--- IntroJavaScrip/Clase9Dom/login.js
+++ IntroJavaScrip/Clase9Dom/login.js
@@ -1,17 +1,39 @@
-function login()
+let seccionPrinmcipal = document.getElementById("seccionPrincipal");
+
+seccionPrincipal.hidden = true;
+
+let usuarioActual = null;
+
+function login(event)
 {
+    event.preventDefault();
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     console.log(username,password);
-
-
+    
+    
     if(username === "felipe@gmail.com" && password === "123")
     {
+        let btnLogin = document.getElementById("btnLogin");
+        btnLogin.disable = true;
+        mensaje.style.color = "green";
         alert("login Exitoso");
-        window.location.href = "index.html";
+        seccionPrincipal.hidden = false;
+        //window.location.href = "index.html";
+        usuarioActual = username;
+        if(usuarioActual != null){
+
+        }
 
     }else
     {
+        mensaje.style.color = "red";
         alert ("usuario y/o contraseña incorrecta");
     }
 }
+
+let btnIngresarDinero = document.getElementById ("ingresarDinero");
+
+btnIngresarDinero.addEventListener("click", function(){
+    alert("hola");
+});
